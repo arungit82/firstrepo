1731d87a6516476611f5910c215f1745c8b063b1
Balamurugan <ps.r.balamurugan@accenture.com>
2016-08-26 18:43:10 +0530

    Change code on 08/26/2016

--- medallion-mgmt/src/main/java/com/carnival/mm/controller/MedallionController.java
+++ medallion-mgmt/src/main/java/com/carnival/mm/controller/MedallionController.java
@@ -2,11 +2,13 @@ package com.carnival.mm.controller;
 
 import com.carnival.mm.domain.Medallion;
 import com.carnival.mm.domain.MedallionAssignment;
+import com.carnival.mm.domain.MedallionTE2;
 import com.carnival.mm.exception.MedallionCannotUpdateException;
 import com.carnival.mm.exception.MedallionNotFoundException;
 import com.carnival.mm.service.MedallionAssignmentTaskService;
 
 import com.carnival.mm.service.MedallionService;
+import com.carnival.mm.service.MedallionTE2Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 import java.util.List;
-
+import java.util.Date;
 /**
  * Created by david.c.hoak on 6/22/2016.
  */
@@ -29,6 +32,8 @@ public class MedallionController {
     @Autowired
     MedallionAssignmentTaskService medallionAssignmentTaskService;
 
+    @Autowired
+    MedallionTE2Service medallionTE2Service;
 
 
     public static final Logger log = LoggerFactory.getLogger(MedallionController.class);
@@ -101,6 +106,23 @@ public class MedallionController {
     }
 
     /**
+     * Endpoint for creating a new medallion instance
+     *
+     * @param medallionTE2
+     * @return
+     */
+    @RequestMapping(value = "/v1/medallion-callback", method = RequestMethod.POST)
+    public void updateMedallionAssignment(@Valid @RequestBody MedallionTE2 medallionTE2) {
+
+
+        // To do: Call back assignment
+        // return medallionService.createMedallion(medallion);
+
+
+    }
+
+
+    /**
      * Endpoint for assigning a medallion to an individual
      * @param medallionAssignment
      * @return
@@ -109,6 +131,8 @@ public class MedallionController {
     public Medallion assignMedallion(@Valid @RequestBody MedallionAssignment medallionAssignment){
 
         Medallion medallion = medallionAssignmentTaskService.assignMedallionToIndividual(medallionAssignment);
+        MedallionTE2 tempMedallionTE2 = getMedallionTE2(medallion);
+        //call --- https://dev-trident.te2.biz/rest/v1/event-subscriptions/event
         return medallion;
     }
 
@@ -118,22 +142,45 @@ public class MedallionController {
      * @return
      */
     @RequestMapping(value="/v1/medallion-GUID/{guestId}", method=RequestMethod.GET)
-   //public Medallion getMedallionGuestID(@Valid @RequestBody Medallion medallion){
-    public Medallion getMedallionGuestID(@PathVariable String guestId){
-
-        String test = guestId;
-        System.out.println("test---------"+test);
-
-        Medallion objMedallion = medallionService.findMedallionByGuestID(guestId);
-        if (objMedallion == null) {
+     public Medallion getMedallionGuestID(@PathVariable String guestId){
+        Medallion medallion = medallionService.findMedallionByGuestID(guestId);
+        if (medallion == null) {
             throw new MedallionNotFoundException(guestId);
         }
-        return objMedallion;
+        return medallion;
 
     }
 
+     private MedallionTE2 getMedallionTE2(Medallion medallion) {
 
-    /**
+        MedallionTE2 medallionTE2 = new MedallionTE2();
+       // Medallion medallion = medallionService.findMedallionByHardwareId(hardwareId);
+
+        if (medallion == null) {
+            //throw new MedallionNotFoundException();
+        }
+        else{
+            medallionTE2.setId(medallion.getId());
+            medallionTE2.setHardwareId(medallion.getHardwareId());
+            medallionTE2.setUiId(medallion.getUuId());
+            medallionTE2.setBleId(medallion.getBleId());
+            medallionTE2.setMajorId(medallion.getMajorId());
+            medallionTE2.setMinorId(medallion.getMinorId());
+            medallionTE2.setUuid(medallion.getUuId());
+            medallionTE2.setNfcId(medallion.getNfcId());
+            medallionTE2.setStatus(medallion.getStatus());
+            medallionTE2.setReservationId(medallion.getReservationId());
+            medallionTE2.set_eventType("MedallionAssignment");
+            medallionTE2.set_operation("create");
+            medallionTE2.set_version("1.0");
+            Date dNow = new Date( );
+            SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd hh:mm:ss");
+            medallionTE2.set_timestamp(ft.format(dNow));
+        }
+        return medallionTE2;
+    }
+
+     /**
      * Endpoint for getting the count of unassigned medallions.  This is a throw away method only to be used for
      * purposes of the release 9 experience slice demo.
      * @return
