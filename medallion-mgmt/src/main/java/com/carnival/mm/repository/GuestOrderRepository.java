9bddd836626537bb8cbe7ddba748ef5a3d872532
d.jayaramaiah <d.jayaramaiah@accenture.com>
2016-10-04 12:58:12 +0530

    Code added for guest order creation.

---
+++ medallion-mgmt/src/main/java/com/carnival/mm/repository/GuestOrderRepository.java
@@ -0,0 +1,13 @@
+package com.carnival.mm.repository;
+
+import com.carnival.mm.domain.GuestOrder;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by d.jayaramaiah on 10/04/2016.
+ */
+public interface GuestOrderRepository extends CrudRepository<GuestOrder, String> {
+
+
+
+}
